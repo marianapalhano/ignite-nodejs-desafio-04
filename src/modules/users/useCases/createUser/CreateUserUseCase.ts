61c3391f0b74ee3f5df2b3bf5b7540ebd90ae635
Gabriela B <gabi.br.dev@gmail.com>
2023-03-02 15:46:44 -0300

    feat(desafio-04): creates user with all props

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -2,16 +2,21 @@ import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  name: string;
-  email: string;
+    name: string;
+    email: string;
 }
 
 class CreateUserUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+    constructor(private usersRepository: IUsersRepository) {}
 
-  execute({ email, name }: IRequest): User {
-    // Complete aqui
-  }
+    execute({ email, name }: IRequest): User {
+        const userAlreadyExists = this.usersRepository.findByEmail(email);
+
+        if (userAlreadyExists != null) {
+            throw new Error("User already exists.");
+        }
+        return this.usersRepository.create({ name, email });
+    }
 }
 
 export { CreateUserUseCase };
